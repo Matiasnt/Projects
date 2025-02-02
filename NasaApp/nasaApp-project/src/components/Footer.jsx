1b8cde7220a7a2931060662cf3201d4d001dab0e
matiasntoledo <34613834+Matiasnt@users.noreply.github.com>
2025-02-01 23:11:41 -0300

    Created Nasa Project

---
+++ NasaApp/nasaApp-project/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div>
+        <h2>The elder scrolls</h2>
+        <h1>APOD PROJECT</h1>
+        <button>
+          <i class="fa-solid fa-circle-info"></i>
+        </button>
+      </div>
+    </footer>
+  );
+}
---
+++ NasaApp/nasaApp-project/src/components/Main.jsx
@@ -0,0 +1,7 @@
+export default function Main() {
+  return (
+    <div>
+        <img src="skyrim.jpg" alt="skyrim" />
+    </div>
+  )
+}
