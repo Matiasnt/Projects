764e35488973753a77250f3bf9c0ed28df52f233
matiasntoledo <34613834+Matiasnt@users.noreply.github.com>
2025-02-03 11:25:47 -0300

    cache + fetchApi

--- NasaApp/nasaApp-project/src/App.jsx
+++ NasaApp/nasaApp-project/src/App.jsx
@@ -3,52 +3,60 @@ import Footer from "./components/Footer";
 import Main from "./components/Main";
 import SideBar from "./components/SideBar";
 
-
 function App() {
- const [data, setData] = useState(null);
- const [loading, setLoading] = useState(false);
- const [showModal, setShowModal] = useState(false);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+  function handleToggleModal() {
+    setShowModal(!showModal);
+  }
+
+  useEffect(() => {
+    async function fetchAPIdata() {
+      const NASA_KEY = import.meta.env.VITE_NASA_KEY;
+      const url =
+        "https://api.nasa.gov/planetary/apod" + `?api_key=${NASA_KEY}`;
 
 
- function handleToggleModal() {
-   setShowModal(!showModal);
- }
+      const today =(new Date()).toDateString();
+      const localKey = `NASA-${today}`;
+      if (localStorage.getItem(localKey)) {
+        const apiData = JSON.parse(localStorage.getItem(localKey));
+        setData(apiData);
+        console.log('Fetched from cache today')
+        return;
+      }
+      localStorage.clear();
+      try {
+        const res = await fetch(url);
+        const apiData = await res.json();
+        localStorage.setItem(localKey, JSON.stringify(apiData));
+        console.log('Fetched from API today')
+        setData(apiData);
+        
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchAPIdata();
+  }, []);
 
-
- useEffect(() => {
-   async function fetchAPIdata() {
-     const NASA_KEY = import.meta.env.VITE_NASA_KEY;
-     const url =
-       "https://api.nasa.gov/planetary/apod" + `?api_key=${NASA_KEY}`;
-
-
-     try {
-       const res = await fetch(url);
-       const apiData = await res.json();
-       setData(apiData);
-       console.log("DATA", apiData);
-     } catch (error) {
-       console.error(error);
-     }
-   }
-   fetchAPIdata();
- }, []);
-
-
- return (
-   <>
-     {data ? (<Main data={data} />): (
-       <div className="loadingState">
-         <i className="fa-solid fa-gear"></i>
-       </div>
-     )}
-     {showModal && <SideBar data={data} handleToggleModal={handleToggleModal} />}
-     {data && (<Footer data={data} handleToggleModal={handleToggleModal} />)}
-   </>
- );
+  return (
+    <>
+      {data ? (
+        <Main data={data} />
+      ) : (
+        <div className="loadingState">
+          <i className="fa-solid fa-gear"></i>
+        </div>
+      )}
+      {showModal && (
+        <SideBar data={data} handleToggleModal={handleToggleModal} />
+      )}
+      {data && <Footer data={data} handleToggleModal={handleToggleModal} />}
+    </>
+  );
 }
 
-
 export default App;
-
-
--- NasaApp/nasaApp-project/src/components/Main.jsx
+++ NasaApp/nasaApp-project/src/components/Main.jsx
@@ -2,7 +2,7 @@ export default function Main(props) {
   const {data} = props; 
   return (
     <div className="imgContainer">
-        <img src={data.hdurl}alt={data.title} className="bgImage"/>
+        <img src={data.hdurl} alt={data.title} className="bgImage"/>
     </div>
   )
  }
